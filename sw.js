292ef4492cb5950ea2a2a05a69c41271ad6af5b9
Hoh Yue <34392514+hoh-yue@users.noreply.github.com>
2026-08-06 00:21:51 +0800

    Resolve offline issue and auto update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shiyue-v21";
+const CACHE_NAME = "shiyue-v23";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", event => {
@@ -12,8 +12,13 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    if (response.ok) {
+      const copy = response.clone();
+      event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)));
+    }
     return response;
-  }).catch(() => caches.match("./index.html"))));
+  }).catch(() => {
+    if (event.request.mode === "navigate") return caches.match("./index.html");
+    return Response.error();
+  })));
 });
